29f06347ee9a4ddf3ac06f606a34a01457e90057
firdanirp <firdani.fr@gmail.com>
2018-05-07 08:16:51 +0700

    HackerRank Loop

---
+++ HackerRank-Loop.js
@@ -0,0 +1,33 @@
+var input = "javascriptloops";
+
+function vowelsAndConsonants(s) {
+    const vowels = 'aeiou';
+    var consonants = '';
+    
+    for(var i = 0; i < s.length; i++) {
+       if (vowels.includes(s[i])) {
+           console.log(s[i]);
+       }
+       else {
+           consonants += s[i] + '\n';
+       }
+    }
+    
+    console.log(consonants.trim());
+}
+
+console.log(vowelsAndConsonants(input));
+
+/*function vowelsAndConsonants(s) {
+    let vowels = ["a", "e", "i", "o", "u"];
+
+    for(let v of s) {
+        if(vowels.includes(v))
+            console.log(v);
+    }
+    
+    for(let v of s) {
+        if(!vowels.includes(v))
+            console.log(v);
+    }
+}*/
