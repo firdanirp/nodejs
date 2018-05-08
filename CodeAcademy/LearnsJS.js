5fa1aa54d502504343d463e5d8a61b0018b93f44
firdanirp <firdani.fr@gmail.com>
2018-05-08 08:24:57 +0700

    Add Learn Code Academy

---
+++ CodeAcademy/LearnsJS.js
@@ -0,0 +1,6 @@
+// This method returns number between 0 and 1 
+console.log(Math.random());
+// This method return number between 0 and 50 
+console.log(Math.random()*50);
+// This method return number rounds down to the nearest whole number
+console.log(Math.floor(Math.random()*50));
