fdf823886e9306c4a428f679963177a522c9f9c0
firdanirp <firdani.fr@gmail.com>
2018-05-11 07:40:10 +0700

    JS Code Acadaemy

---
+++ CodeAcademy/Classes.js
@@ -0,0 +1,58 @@
+class HospitalEmployee {
+    constructor(name) {
+      this._name = name;
+      this._remainingVacationDays = 20;
+    }
+    
+    get name() {
+      return this._name;
+    }
+    
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+    
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff;
+    }
+  }
+  
+  class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+      super(name);
+      this._certifications = certifications;
+    } 
+    
+    get certifications() {
+      return this._certifications;
+    }
+    
+    addCertification(newCertification) {
+      this.certifications.push(newCertification);
+    }
+  }
+  
+  const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+  nurseOlynyk.takeVacationDays(5);
+  console.log(nurseOlynyk.remainingVacationDays);
+  nurseOlynyk.addCertification('Genetics');
+  console.log(nurseOlynyk.certifications);
+
+  /*Way to go! Let's review what you learned.
+
+Classes are templates for objects.
+Javascript calls a constructor method when we create a new instance of a class.
+Inheritance is when we create a parent class with properties and methods that we can extend to child classes.
+We use the extends keyword to create a subclass.
+The super keyword calls the constructor() of a parent class.
+Static methods are called on the class, but not on instances of the class.
+In completing this lesson, you've taken one step closer to writing efficient, production-level JavaScript. Good luck as you continue to develop your skills and move into intermediate-level concepts.
+
+If you want extra practice with inheritance, take some time to create a Doctor class that inherits from HospitalEmployee. The properties and methods for the Doctor class are listed below:
+
+DOCTOR
+properties: _name, _remainingVacationDays (set to 20 inside constructor()), _insurance
+methods: .takeVacationDays()
+
+
+*/
